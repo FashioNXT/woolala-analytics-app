f683e382b779d2212e07007e1e6d877b770e3420
Devesh Kumar <devesh@tamu.edu>
2021-04-15 03:43:22 -0500

    Login , register ,all_data features and templates added

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ from flask import Flask , render_template, url_for, request, session, redirect
 from blueprints.admin_app_blueprint import admin_app_page
 import bcrypt
 import os
-from flask import g
+from flask import g, current_app
 import configparser
 from database_connection.database_connection import get_mongo_database
 
 template_dir = "frontend/templates"
 app = Flask(__name__, template_folder=template_dir)
-
+app.secret_key = "super secret key"
 
 
 
@@ -22,17 +22,17 @@ def index():
 def set_database_config():
     config = configparser.RawConfigParser()
     config.read('ConfigFile.properties')
-    g.env = config.get('General', 'env')
-    g.database_url = config.get('DatabaseSection', 'database.url')
-    g.db_name = config.get('DatabaseSection', 'database.prod')
-    if (g.env == "Test"):
-        g.db_name = config.get('DatabaseSection', 'database.test')
+    current_app.env = config.get('General', 'env')
+    current_app.database_url = config.get('DatabaseSection', 'database.url')
+    current_app.db_name = config.get('DatabaseSection', 'database.prod')
+    if (current_app.env == "test"):
+        current_app.db_name = config.get('DatabaseSection', 'database.test')
 
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
     with app.app_context():
         set_database_config()
-        g.db = get_mongo_database()
+        current_app.db = get_mongo_database()
     app.register_blueprint(admin_app_page)
-    app.run(threaded=True, port=5000 )
+    app.run(threaded=True, port=5000 ,debug=True )
--- database_connection/database_connection.py
+++ database_connection/database_connection.py
@@ -1,11 +1,11 @@
 from pymongo import MongoClient
 import configparser
 import base64
-from flask import g
+from flask import g,current_app
 def get_mongo_database():
 
-    client = MongoClient(g.database_url)
-    db = client[g.db_name]
+    client = MongoClient(current_app.database_url)
+    db = client[current_app.db_name]
 
     return db
 
